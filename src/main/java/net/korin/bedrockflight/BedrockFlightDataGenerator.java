783ea76002f5904a4dc552532983d4063c42cbd3
KorinDev <92170697+KorinDev@users.noreply.github.com>
2026-06-01 02:46:14 +0200

    Initial Commit

---
+++ src/main/java/net/korin/bedrockflight/BedrockFlightDataGenerator.java
@@ -0,0 +1,11 @@
+package net.korin.bedrockflight;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BedrockFlightDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
