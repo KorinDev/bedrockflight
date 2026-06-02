a9a325249eea1a8436109551d35276c373951a4b
KorinDev <92170697+KorinDev@users.noreply.github.com>
2026-06-02 14:04:20 +0200

    Added slight deceleration and changed Inject from HEAD to TAIL.

--- src/main/java/net/korin/bedrockflight/mixin/CreativeFlightMixin.java
+++ src/main/java/net/korin/bedrockflight/mixin/CreativeFlightMixin.java
@@ -1,52 +1,39 @@
 package net.korin.bedrockflight.mixin;
 
-import net.minecraft.world.entity.player.Abilities;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.Vec3;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import javax.swing.*;
+
 @Mixin(Player.class)
 public abstract class CreativeFlightMixin {
 
-    private double originalY = 0;
+    private static final float DECELERATION_FACTOR = 0.65f;
 
-    @Shadow
-    @Final
-    private Abilities abilities;
 
-    @Shadow
-    public abstract Abilities getAbilities();
-
-    @Shadow
-    public abstract boolean isCreative();
-
-    @Inject(method = "travel", at = @At("HEAD"))
+    @Inject(method = "travel", at = @At("TAIL"))
     private void bedrockCreativeFlight(Vec3 input, CallbackInfo ci) {
         Player player = (Player)(Object)this;
 
-        if (player.getAbilities().flying && player.isCreative() && !player.isInWater()) {
-            originalY = player.getDeltaMovement().y;
-
+        if (player.getAbilities().flying && player.isCreative() && player.isLocalPlayer()) {
             player.getAbilities().setFlyingSpeed(0.06f);
 
             if (input.x == 0 && input.z == 0) {
-                player.setDeltaMovement(0, player.getDeltaMovement().y, 0);
+                //player.setDeltaMovement(0, player.getDeltaMovement().y, 0);
+                Vec3 current = player.getDeltaMovement();
+
+                double newX = current.x * DECELERATION_FACTOR;
+                double newZ = current.z * DECELERATION_FACTOR;
+
+                if (Math.abs(newX) < 0.001) newX = 0;
+                if (Math.abs(newZ) < 0.001) newZ = 0;
+
+                player.setDeltaMovement(newX, current.y, newZ);
             }
         }
     }
-
-    /*@Inject(method = "travel", at = @At("RETURN"))
-    private void onTravelEnd(Vec3 input, CallbackInfo ci) {
-        Player player = (Player)(Object)this;
-
-        if (player.getAbilities().flying && player.isCreative()) {
-            Vec3 currentMotion = player.getDeltaMovement();
-            player.setDeltaMovement(currentMotion.x, originalY * 0.6, currentMotion.z);
-        }
-    }*/
 }
